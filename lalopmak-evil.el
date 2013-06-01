01017058662c9436355cbdf93cdfaf6b16cf5eab
mastar sing <lalop.lmao@gmail.com>
2013-06-01 01:21:26 -0500

    renamed colemak.evil

--- lalopmak-evil.el
+++ lalopmak-evil.el
@@ -1,20 +1,23 @@
-;;; License
-    
-;; Colemak Evil: A set of optimized Vim-like key bindings for Emacs.
-;; Copyright (C) 2013 Patrick Brinich-Langlois
-;; 
-;; This program is free software: you can redistribute it and/or modify
-;; it under the terms of the GNU General Public License as published by
-;; the Free Software Foundation, either version 3 of the License, or
-;; (at your option) any later version.
-;; 
-;; This program is distributed in the hope that it will be useful,
-;; but WITHOUT ANY WARRANTY; without even the implied warranty of
-;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-;; GNU General Public License for more details.
-;; 
-;; For the full text of the GNU General Public License, see
-;; http://www.gnu.org/licenses/gpl.html 
+
+;;  Colemak Evil: A set of optimized Vim-like key bindings for Emacs.
+;;  Copyright (C) 2013 Patrick Brinich-Langlois
+
+;;  lalopmak-evil: A more geometric fork.
+;;  Copyright (C) 2013
+
+;;  This program is free software: you can redistribute it and/or modify
+;;  it under the terms of the GNU General Public License as published by
+;;  the Free Software Foundation, either version 3 of the License, or
+;;  (at your option) any later version.
+
+;;  This program is distributed in the hope that it will be useful,
+;;  but WITHOUT ANY WARRANTY; without even the implied warranty of
+;;  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;;  GNU General Public License for more details.
+
+;;  You should have received a copy of the GNU General Public License
+;;  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 
 (defvar lalopmak-evil-hintstring "Hints for lalop's colemak-evil configuration.  Accessed via: :hints, :h, :ars, or M-x lalopmak-evil-hints.
 
