89e20bd0466ccfdfbb7704363297295a1f5d72a6
lalopmak <lalop.llmao@gmail.com>
2013-08-17 18:29:39 -0500

    Corrected typo in select

--- lalopmak-evil-base.el
+++ lalopmak-evil-base.el
@@ -125,8 +125,8 @@ adds 'motion bindings to that lisp mode map."
 
 (lalopmak-evil-define-lisp-motions "  " (lambda () (interactive) (insert " "))  ;;two spaces for a space
 
-                                   " a" "cs(" ;;select outside parens
-                                   " A" "cr(" ;;select inside parens
+                                   " a" "as(" ;;select outside parens
+                                   " A" "ar(" ;;select inside parens
 
                                    " c" "cs(" ;;copy outside parens
                                    " C" "cr(" ;;copy inside parens
